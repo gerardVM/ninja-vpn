8a5a206446c6488306ac6bd591f3e05aa3b89f49
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-06-03 21:09:59 +0200

    Update action.sh

--- ops/scripts/action.sh
+++ ops/scripts/action.sh
@@ -7,6 +7,15 @@ case $(cat ./users/${1}.yaml | yq -r .action) in
     destroy)
         make vpn-destroy USER=${1}
         ;;
+    init)
+        make tf-init USER=${1}
+        ;;
+    validate)
+        make tf-validate USER=${1}
+        ;;
+    plan)
+        make tf-plan USER=${1}
+        ;;
     *)
         echo "Usage: $0 {deploy|destroy}"
         exit 1
