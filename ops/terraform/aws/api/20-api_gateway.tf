de6dc36c62fc50e9b05dbe1fade037b1a796302b
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-09-29 20:09:59 +0200

    Adding CORS configuration. Not working yet

--- ops/terraform/aws/api/20-api_gateway.tf
+++ ops/terraform/aws/api/20-api_gateway.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "lambda"
+  path_part   = "{cors+}"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
@@ -11,8 +11,24 @@ resource "aws_api_gateway_resource" "resource" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
+  # http_method   = "POST"
+  http_method   = "OPTIONS"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+  }
+  
+  depends_on = [aws_api_gateway_method.method]
 }
 
 resource "aws_api_gateway_integration" "integration" {
@@ -32,8 +48,50 @@ resource "aws_api_gateway_integration" "integration" {
   }
 }
 
+resource "aws_api_gateway_integration_response" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = aws_api_gateway_method_response.cors.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'", # Change
+  }
+}
+
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "launch"
+}
+
+resource "aws_api_gateway_gateway_response" "response_4xx" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  # response_type = "ACCESS_DENIED"
+  response_type = "DEFAULT_4XX"
+  status_code   = "404"
+  response_templates = {
+    "application/json" = jsonencode({  # Change for something more professional
+      "message" = "Not Found"          
+    })
+  }
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'", # Change
+  }
+}
+
+resource "aws_api_gateway_gateway_response" "response_5xx" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  response_type = "DEFAULT_5XX"
+  status_code   = "500"
+  response_templates = {
+    "application/json" = jsonencode({  # Change for something more professional
+      "message" = "Internal Server Error"          
+    })
+  }
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'", # Change
+  }
 }
--- ops/terraform/aws/api/30-S3.tf
+++ ops/terraform/aws/api/30-S3.tf
@@ -1,3 +1,38 @@
 resource "aws_s3_bucket" "bucket" {
     bucket = local.config.bucket_name
 }
+
+
+resource "aws_s3_bucket" "site" {
+    bucket = "vpnn-site"
+}
+
+resource "aws_s3_object" "index" {
+    bucket = aws_s3_bucket.site.id
+    key    = "index.html"
+    source = "../../../../sites/frontend/index.html"
+    content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "allow_site_access" {
+    bucket = aws_s3_bucket.site.id
+    policy = jsonencode({
+        Statement = [
+            {
+                Sid       = "PublicReadGetObject"
+                Action    = "s3:GetObject"
+                Effect    = "Allow"
+                Principal = "*"
+                Resource  = "arn:aws:s3:::vpnn-site/*"
+            },
+        ]
+        Version   = "2012-10-17"
+    })
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+    bucket = aws_s3_bucket.site.id
+    index_document {
+        suffix = "index.html"
+    }
+}
