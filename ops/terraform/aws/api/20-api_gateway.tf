ea67bfe5c06f13cc78b5c0a28645f6d55aaabb44
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-09-29 23:06:13 +0200

    More configuration. More testing

--- ops/terraform/aws/api/20-api_gateway.tf
+++ ops/terraform/aws/api/20-api_gateway.tf
@@ -60,7 +60,7 @@ resource "aws_api_gateway_integration" "cors" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.cors.http_method
-  integration_http_method = "OPTIONS"
+  # integration_http_method = "OPTIONS"
   type                    = "MOCK"
   request_templates = {
     "application/json" = jsonencode({  # Assuming the Lambda expects JSON data
