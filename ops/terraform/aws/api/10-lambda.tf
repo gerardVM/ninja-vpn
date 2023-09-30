0489e814102aabe6590644bba92d1a8178caa143
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-09-30 13:44:28 +0200

    This is finally working. Time to do tests

--- ops/terraform/aws/api/10-lambda.tf
+++ ops/terraform/aws/api/10-lambda.tf
@@ -74,5 +74,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${local.api_region}:${local.api_accountId}:${aws_api_gateway_rest_api.api.id}/*"
+  source_arn = "arn:aws:execute-api:${local.api_region}:${local.api_accountId}:${aws_apigatewayv2_api.api.id}/*"
 }
