1b73803e245fec6e89562fe454dea5f209f06ff6
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-10-01 19:07:45 +0200

    Hotfix 80-countdown.tf [skip ci]

--- ops/terraform/aws/vpn/80-countdown.tf
+++ ops/terraform/aws/vpn/80-countdown.tf
@@ -8,7 +8,7 @@ module "remote_termination" {
   eip_id         = aws_eip.eip.id
   sender_email   = local.config.ses_sender_email
   receiver_email = local.config.email # aws_sesv2_email_identity.email_notifications.email_identity if your account is in the Amazon SES sandbox
-  ses_region     = local.config.region
+  ses_region     = local.config.api_region
   
   tags           = {
     Name = local.config.name
