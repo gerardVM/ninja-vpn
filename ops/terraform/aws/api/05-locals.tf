a85ab19cf94a42cc99480cf15e77205ff99df6b1
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-10-04 07:43:17 +0200

    Fixing bugs

--- ops/terraform/aws/api/05-locals.tf
+++ ops/terraform/aws/api/05-locals.tf
@@ -1,5 +1,6 @@
 locals {
     config        = yamldecode(file("${path.root}/../../../../config.yaml"))
+    index_path    = "${path.module}/../../../../site/frontend/index_template.html"
     api_region    = local.config.api_region
     api_accountId = local.config.account
 
