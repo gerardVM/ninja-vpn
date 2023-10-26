aff2adc5f5df1590307c8e29d95c328a2a2ad83d
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-10-26 23:11:00 +0200

    Bringing back alternate domain

--- ops/terraform/aws/api/60-route53.tf
+++ ops/terraform/aws/api/60-route53.tf
@@ -18,7 +18,7 @@ resource "aws_route53_record" "validation" {
 
 # resource "aws_route53_record" "all_subdomains" {
 #   name   = "*.${local.domain_name}"
-#   type   = "A"
+#   type   = "CNAME"
 #   zone_id = aws_route53_zone.hosted_zone.zone_id
 #   alias {
 #     name                   = aws_cloudfront_distribution.distribution.domain_name
@@ -27,13 +27,17 @@ resource "aws_route53_record" "validation" {
 #   }
 # }
 
-# resource "aws_route53_record" "root_domain" {
-#   name   = local.domain_name
-#   type   = "A"
-#   zone_id = aws_route53_zone.hosted_zone.zone_id
-#   alias {
-#     name                   = aws_cloudfront_distribution.distribution.domain_name
-#     zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
-#     evaluate_target_health = false
-#   }
+resource "aws_route53_record" "root_domain" {
+  name   = local.domain_name
+  type   = "A"
+  zone_id = aws_route53_zone.hosted_zone.zone_id
+  alias {
+    name                   = aws_cloudfront_distribution.distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+# output "cloudfront_domain_name" {
+#   value = aws_route53_record.root_domain.name
 # }
