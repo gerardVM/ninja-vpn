c6b1d070f84dfcece60932682795988be3174420
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-10-29 11:37:08 +0100

    Adding subdomains to ssl certificate (#27)

--- ops/terraform/aws/api/70-acm.tf
+++ ops/terraform/aws/api/70-acm.tf
@@ -1,6 +1,7 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "*.${aws_route53_zone.hosted_zone.name}"
-  validation_method = "DNS"
+  domain_name               = aws_route53_zone.hosted_zone.name
+  subject_alternative_names = ["*.${aws_route53_zone.hosted_zone.name}"]
+  validation_method         = "DNS"
 
   tags = {
     Environment = "staging"
