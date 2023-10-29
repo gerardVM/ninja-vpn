fe46e7761be83a3a139083d44fcdb0861a8dbf23
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-10-29 11:33:38 +0100

    Polishing validation record

--- ops/terraform/aws/api/60-route53.tf
+++ ops/terraform/aws/api/60-route53.tf
@@ -1,20 +1,27 @@
+locals {
+  unique_dvos = distinct(toset(flatten([
+    for dvo in aws_acm_certificate.cert.domain_validation_options : {
+      name        = dvo.resource_record_name
+      value       = dvo.resource_record_value
+      type        = dvo.resource_record_type
+      zone_id     = aws_route53_zone.hosted_zone.zone_id
+      domain_name = aws_route53_zone.hosted_zone.name
+    }
+  ])))
+}
+
 resource "aws_route53_zone" "hosted_zone" {
   name = local.domain_name
 }
 
 resource "aws_route53_record" "validation" {
-  for_each = { for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-    name    = dvo.resource_record_name
-    value   = dvo.resource_record_value
-    type    = dvo.resource_record_type
-    zone_id = aws_route53_zone.hosted_zone.zone_id
-  } }
+  for_each = { for dvo in local.unique_dvos : dvo.domain_name => dvo }
   name    = each.value.name
   type    = each.value.type
   zone_id = each.value.zone_id
   records = [each.value.value]
   ttl     = 60
-  }
+}
 
 resource "aws_route53_record" "root_domain" {
   name   = aws_route53_zone.hosted_zone.name
