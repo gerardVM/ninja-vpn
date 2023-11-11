63d83cd16bdfb9a57f10ee82b6b219b47ce41ed1
gerardVM <101387700+gerardVM@users.noreply.github.com>
2023-11-11 23:28:33 +0100

    Quick fix

--- ops/terraform/aws/vpn/20-iam.tf
+++ ops/terraform/aws/vpn/20-iam.tf
@@ -69,5 +69,4 @@ EOF
 resource "aws_iam_role_policy_attachment" "fleet_role_policy_attachment" {
   role       = aws_iam_role.fleet_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole"
-  depends_on = [aws_iam_role_policy_attachment.ec2_role_policy_attachment]
 }
