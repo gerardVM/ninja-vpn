580767e460602776c5de83ab074aadb7de91623f
gerardVM <101387700+gerardVM@users.noreply.github.com>
2024-02-18 17:03:47 +0100

    Hashing quick fix (#41)

--- ops/terraform/aws/api/90-dynamodb.tf
+++ ops/terraform/aws/api/90-dynamodb.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "authorized_users" {
   name           = "ninja-vpn-authorized_users"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "email"
+  hash_key       = "email_hash"
 
   attribute {
-    name = "email"
+    name = "email_hash"
     type = "S"
   }
 
